f2fb24170a4a3267f8cd16a9b29236395c772280
skroppy04-art <skroppy04@gmail.com>
2026-04-07 21:50:14 +0300

    Refactor app.py to simplify database connection and add test route

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, request, jsonify
-import pymysql
-import bcrypt
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-db = pymysql.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS"),
-    database=os.getenv("DB_NAME")
-)
-
 @app.route('/')
 def home():
-    return "API работает"
+    return "OK WORKING"
+
+@app.route('/test')
+def test():
+    return "TEST OK"
+
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
