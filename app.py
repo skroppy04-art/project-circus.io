961cb660b8492959aa0968dacddc3e61606131a1
skroppy04-art <skroppy04@gmail.com>
2026-04-07 21:48:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,6 @@ db = pymysql.connect(
     database=os.getenv("DB_NAME")
 )
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    username = data['username'].lower()
-    password = data['password']
-
-    cursor = db.cursor()
-    cursor.execute("SELECT password FROM authme WHERE username=%s", (username,))
-    user = cursor.fetchone()
-
-    if user and bcrypt.checkpw(password.encode(), user[0].encode()):
-        return jsonify({"status": "ok"})
-    
-    return jsonify({"status": "error"})
-
-app.run(host="0.0.0.0", port=10000)
-
 @app.route('/')
 def home():
     return "API работает"
